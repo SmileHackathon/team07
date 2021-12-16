2669c78991dab4fd542478a87d29a896982af677
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-12-16 19:59:31 +0900

    作業途中

--- app/src/main/java/com/example/silent_life/MainActivity.kt
+++ app/src/main/java/com/example/silent_life/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.silent_life
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    // キーボード表示を制御するためのオブジェクト
+    private lateinit var inputMethodManager: InputMethodManager
+    // 背景のレイアウトを取得
+    var container:ConstraintLayout = findViewById(R.id.container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // InputMethodManagerを取得
+        inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        // キーボードを隠す
+        inputMethodManager.hideSoftInputFromWindow(container.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+
+        // 背景にフォーカスを移す
+        container.requestFocus()
+
+        return false
     }
 }
+
