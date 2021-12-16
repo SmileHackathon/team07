011e836bdbeed4276d4cbabc488461c11dcd128f
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2021-12-16 20:59:16 +0900

    背景をタップした時、エンターキーを押した時の処理が完了

--- app/src/main/java/com/example/silent_life/MainActivity.kt
+++ app/src/main/java/com/example/silent_life/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.silent_life
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
 
 
@@ -13,14 +16,32 @@ import androidx.constraintlayout.widget.ConstraintLayout
 class MainActivity : AppCompatActivity() {
     // キーボード表示を制御するためのオブジェクト
     private lateinit var inputMethodManager: InputMethodManager
-    // 背景のレイアウトを取得
-    var container:ConstraintLayout = findViewById(R.id.container)
+    // 背景のレイアウト
+    lateinit var container:ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //背景のレイアウトを取得
+        container = findViewById(R.id.container)
         // InputMethodManagerを取得
         inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val editText:EditText= findViewById(R.id.editText)
+
+        editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                //Perform Code
+
+                // キーボードを隠す
+                inputMethodManager.hideSoftInputFromWindow(container.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+
+                // 背景にフォーカスを移す
+                container.requestFocus()
+
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -33,5 +54,7 @@ class MainActivity : AppCompatActivity() {
 
         return false
     }
+
+
 }
 
